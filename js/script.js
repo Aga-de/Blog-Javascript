3a11811b1cae3757b82d185e250dbb190d1132dc
Aga Daniuk <agdani@wp.pl>
2020-11-12 13:46:22 +0100

    Complete event handler

--- js/script.js
+++ js/script.js
@@ -6,10 +6,11 @@
   });*/
 
   const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log(event);
     console.log('Link was clicked!');
-
+    
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -17,7 +18,7 @@
     activeLink.classList.remove('active');
     }
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
 
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
@@ -29,11 +30,16 @@
     activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute("href");
+    console.log(articleSelector);
 
-    /* find the correct article using the selector (value of 'href' attribute) */
-
-    /* add class 'active' to the correct article */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+  
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
