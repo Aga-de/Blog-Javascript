3311d15e169a306c3f698deb04c9587ef0d20248
Aga Daniuk <agdani@wp.pl>
2020-11-13 18:33:04 +0100

    Add title list

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
 'use strict'
 
-/*document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-  });*/
-
-  const titleClickHandler = function(event){
+{   const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
     console.log(event);
@@ -43,7 +38,55 @@
   }
   
   const links = document.querySelectorAll('.titles a');
+  console.log(links);
   
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+const optArticleSelector = '.post',
+optTitleSelector = '.post-title',
+optTitleListSelector = '.titles';
+
+const generateTitleLinks = function(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector); 
+
+    titleList.innerHTML = '';
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  /* find all the articles and save them to variable: articles */
+  let html = '';
+
+  for (let article of articles){
+    /* get the article id */
+    const articleId = article.getAttribute("id");
+
+    /* find the title element  &  get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' +  articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+
+    /* insert link into html variable */
+    html = html + linkHTML;
+    console.log(html);
+  }
+
+  titleList.innerHTML = html;
+
+  const links = document.querySelectorAll('.titles a');
+  console.log(links);
+  
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
+}
+
+generateTitleLinks();
+
+}
