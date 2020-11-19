3316fe6bc1fcc8115bde20880b98ed8a6f3c5c2f
Aga Daniuk <agdani@wp.pl>
2020-11-19 10:24:23 +0100

    Generate tags

--- js/script.js
+++ js/script.js
@@ -45,7 +45,8 @@
 
 const optArticleSelector = '.post',
 optTitleSelector = '.post-title',
-optTitleListSelector = '.titles';
+optTitleListSelector = '.titles',
+optArticleTagsSelector = '.post-tags .list';
 
 const generateTitleLinks = function(){
 
@@ -85,8 +86,54 @@ const generateTitleLinks = function(){
     link.addEventListener('click', titleClickHandler);
   }
 }
-
 generateTitleLinks();
 
+
+
+const generateTags = function() {
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  /* START LOOP: for every article: */
+    for (let article of articles) {
+
+    /* find tags wrapper */
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
+    console.log(tagsWrapper);
+
+    /* make html variable with empty string */
+    let html = '';
+
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute("data-tags");
+    console.log(articleTags);
+
+    /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+    console.log(articleTagsArray);
+    
+    /* START LOOP: for each tag */
+    for (let tag of articleTagsArray) {
+      console.log(tag);
+
+      /* generate HTML of the link */
+      const tagHTML = '<li><a href="#tag-' + tag + '"><span>' +  tag + '</span></a></li>';
+      console.log(tagHTML);
+      
+      /* add generated code to html variable */
+      html = html + tagHTML;
+      console.log(html);
+
+    /* END LOOP: for each tag */
+    }
+
+    /* insert HTML of all the links into the tags wrapper */
+    tagsWrapper.innerHTML = html;
+
+  /* END LOOP: for every article: */
+    }
+      
 }
 
+generateTags();
+}
